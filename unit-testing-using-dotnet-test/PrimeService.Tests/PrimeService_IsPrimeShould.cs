14fd0ebec17acdf56b924339022a85497037094b
Jadabyte <Jadabyte>
2020-10-14 13:43:07 +0200

    xUnit test tutorial added to branch Thibaud

---
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
