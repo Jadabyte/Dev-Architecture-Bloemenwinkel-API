8a1c69eccc738080926600e9a9ba7cf3d21b5b41
BlackElias <valienne.elias@outlook.com>
2020-10-21 10:34:32 +0200

    added models/proudcts/controller

---
+++ Controller/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using Dev-Architecture - Bloemenwinkel - API.Models;
+public class ProductsController : ApiController
+{
+    static readonly IProductRepository repository = new ProductRepository();
+
+}
+//Get all products
+public class ProductsController : ApiController
+{
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return repository.GetAll();
+    }
+    
+}
+//Get products By id
+public Product GetProduct(int id)
+{
+    Product item = repository.Get(id);
+    if (item == null)
+    {
+        throw new HttpResponseException(HttpStatusCode.NotFound);
+    }
+    return item;
+}
+//Get products By Category
+public IEnumerable<Product> GetProductsByCategory(string category)
+{
+    return repository.GetAll().Where(
+        p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+}
+// creates new products
+
+
+public HttpResponseMessage PostProduct(Product item)
+{
+    item = repository.Add(item);
+    var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+    string uri = Url.Link("DefaultApi", new { id = item.Id });
+    response.Headers.Location = new Uri(uri);
+    return response;
+}
+//update product
+public void PutProduct(int id, Product product)
+{
+    product.Id = id;
+    if (!repository.Update(product))
+    {
+        throw new HttpResponseException(HttpStatusCode.NotFound);
+    }
+
+}
+
+//delete products
+public void DeleteProduct(int id)
+{
+    Product item = repository.Get(id);
+    if (item == null)
+    {
+        throw new HttpResponseException(HttpStatusCode.NotFound);
+    }
+
+    repository.Remove(id);
+}
---
+++ Models/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿namespace Dev-Architecture-Bloemenwinkel-API.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> products = new List<Product>();
+        private int _nextId = 1;
+
+        public ProductRepository()
+        {
+        Add(new Product { Name = "Roos", Category = "bloem", Price = 1.39 });
+        Add(new Product { Name = "Agapanthus", Category = "bloem", Price = 4.99 });
+        Add(new Product { Name = "Tulp", Category = "bloem", Price = 3.75 });
+        Add(new Product { Name = "Zonnebloem", Category = "bloem", Price = 6.99 });
+        Add(new Product { Name = "Camelia", Category = "bloem", Price = 3.99 });
+    }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public Product Add(Product item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            item.Id = _nextId++;
+            products.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Product item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            int index = products.FindIndex(p => p.Id == item.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            products.RemoveAt(index);
+            products.Add(item);
+            return true;
+        }
+    }
+}
