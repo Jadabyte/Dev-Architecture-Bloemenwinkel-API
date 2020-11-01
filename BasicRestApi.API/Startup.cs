b13e17aa96f118231ffe1ae0932b3c2fe6023e2b
Jadabyte <Jadabyte>
2020-11-01 09:22:53 +0100

    commit for merge

--- BasicRestApi.API/Startup.cs
+++ BasicRestApi.API/Startup.cs
@@ -26,6 +26,7 @@ namespace BasicRestApi.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["ConnectionStrings:Default"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
