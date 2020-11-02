9785972b0be4ff11214621e8b51ff51533002ba9
BlackElias <valienne.elias@outlook.com>
2020-11-02 19:03:27 +0100

    meer fixes

--- BasicRestApi.API/Controllers/StoreController.cs
+++ BasicRestApi.API/Controllers/StoreController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BasicRestApi.Model.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
