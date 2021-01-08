bb567011aeeba31bd0b12cec8d53e5d12d026cb7
BlackElias <valienne.elias@outlook.com>
2021-01-08 16:49:38 +0100

    eliaas code api/vlaandere

--- BasicRestApi.API/WeatherForecast.cs
+++ BasicRestApi.API/WeatherForecast.cs
@@ -13,3 +13,45 @@ namespace BasicRestApi.API
         public string Summary { get; set; }
     }
 }
+//werkt niet
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+ using app.UseRouting();
+//ik weet niet wat dit doet 
+
+//controller steken
+
+  
+} 
+static readonly HttpClient client = new HttpClient();
+[Route("https://api.basisregisters.vlaanderen.be/v1/adressen/")]
+//zoekt api
+class controller :
+
+   
+   
+
+  //antwoord juist
+  try
+  {
+    [HttpGet]
+  void Get(int huisnummer, String straatnaam, int postcode) {} 
+
+     response.EnsureSuccessStatusCode();
+     string responseBody = await response.Content.ReadAsStringAsync();
+
+
+     Console.WriteLine(responseBody);
+
+        public string volledigAdres { get; set; }
+  }
+//antwoord fout
+  catch(HttpRequestException e)
+  {
+    public string detail { get; set; }
+     Console.WriteLine("\nException Caught!");
+     Console.WriteLine("Message :{0} ",e.Message);
+  }
+
+}
